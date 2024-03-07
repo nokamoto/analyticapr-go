ce977d4e104cea02043c21aabf13e3d7ea548beb
Naofumi Okamoto <nokamoto.engr@gmail.com>
2024-03-07 14:13:16 +0000

    Add protocol buffers

--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
 )
@@ -13,6 +15,21 @@ func Goimports() error {
 	return g0("run", "golang.org/x/tools/cmd/goimports", "-w", ".")
 }
 
+// Buf runs the buf command with the given subcommand.
+func Buf(cmd string) error {
+	args := []string{"run", "github.com/bufbuild/buf/cmd/buf"}
+	switch cmd {
+	case "generate":
+		args = append(args, cmd, "--template", "build/buf.gen.yaml")
+	case "format":
+		args = append(args, cmd, "-w")
+	default:
+		fmt.Println("buf: unknown supported subcommand:", cmd)
+		return nil
+	}
+	return g0(args...)
+}
+
 func Tidy() error {
 	return g0("mod", "tidy")
 }
@@ -23,7 +40,14 @@ func Test() error {
 
 // Build runs all the build steps.
 func Build() error {
+	s1 := func(s string, f func(string) error) func() error {
+		return func() error {
+			return f(s)
+		}
+	}
 	mg.SerialDeps(
+		s1("format", Buf),
+		s1("generate", Buf),
 		Goimports,
 		Tidy,
 	)
--- tools/tools.go
+++ tools/tools.go
@@ -3,6 +3,8 @@
 package tools
 
 import (
+	_ "github.com/bufbuild/buf/cmd/buf"
 	_ "github.com/magefile/mage"
 	_ "golang.org/x/tools/cmd/goimports"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
